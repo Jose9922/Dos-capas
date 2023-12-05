6aecd68fcd96cf3c52916880e5bea4f8c7cc41b6
jose <josemlaguadoc@gmail.com>
2023-12-05 18:01:41 -0500

    proyecto

---
+++ ejerciocio de dos capas/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ejerciocio_de_dos_capas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtpromedio = new System.Windows.Forms.TextBox();
+            this.txtcondicion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(80, 184);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "CALCULAR";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(215, 184);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnnuevo.TabIndex = 1;
+            this.btnnuevo.Text = "NUEVO";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            this.btnnuevo.Click += new System.EventHandler(this.btnnuevo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nota1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nota2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nota3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "El promedio es:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Condicion:";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(215, 36);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 20);
+            this.txtNota1.TabIndex = 7;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(215, 84);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 20);
+            this.txtNota2.TabIndex = 8;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(215, 127);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 20);
+            this.txtNota3.TabIndex = 9;
+            // 
+            // txtpromedio
+            // 
+            this.txtpromedio.Location = new System.Drawing.Point(218, 250);
+            this.txtpromedio.Name = "txtpromedio";
+            this.txtpromedio.Size = new System.Drawing.Size(100, 20);
+            this.txtpromedio.TabIndex = 10;
+            // 
+            // txtcondicion
+            // 
+            this.txtcondicion.Location = new System.Drawing.Point(218, 314);
+            this.txtcondicion.Name = "txtcondicion";
+            this.txtcondicion.Size = new System.Drawing.Size(100, 20);
+            this.txtcondicion.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 450);
+            this.Controls.Add(this.txtcondicion);
+            this.Controls.Add(this.txtpromedio);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnnuevo);
+            this.Controls.Add(this.btncalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtpromedio;
+        private System.Windows.Forms.TextBox txtcondicion;
+    }
+}
+
---
+++ ejerciocio de dos capas/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejerciocio_de_dos_capas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double Nota_1, Nota_2, Nota_3, promedio;
+            string condicion;
+
+            Nota_1 = double.Parse(txtNota1.Text);
+            Nota_2 = double.Parse(txtNota2.Text);
+            Nota_3= double.Parse(txtNota3.Text);
+
+            promedio = (Nota_1 + Nota_2 + Nota_3) / 3;
+
+            if (promedio <= 29) 
+            {
+                condicion = "DESAPROVADO";
+                txtpromedio.Text = promedio.ToString("N2");
+                txtcondicion.Text = condicion;
+            }else if (promedio > 30 && promedio <= 50)
+            {
+                condicion = "APROVADO";
+                txtpromedio.Text = promedio.ToString("N2");
+                txtcondicion.Text = condicion;
+            }
+        }
+
+        private void btnnuevo_Click(object sender, EventArgs e)
+        {
+            txtNota1.Clear();
+            txtNota2.Clear();
+            txtNota3.Clear();
+            txtpromedio.Clear();
+            txtcondicion.Clear();
+            txtNota1.Focus();
+        }
+    }
+}
